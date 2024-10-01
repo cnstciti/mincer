f9aac7a627eb5d6d2a95f05f2dec7c56feccf99e
Constantin Ogloblin <cnst@mail.ru>
2024-10-01 08:38:07 +0300

    first commit

---
+++ common/config/main.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'language'   => 'ru-RU', // язык приложения
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => \yii\caching\FileCache::class,
+        ],
+    ],
+];
